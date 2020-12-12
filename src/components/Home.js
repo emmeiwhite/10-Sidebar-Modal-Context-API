bc7e8d5b7afe762d2f7eb110279dac3889a581ac
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-12-12 15:23:50 +0530

    Bug fixed, Component sidebar is ready

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,12 @@ import Modal from "./Modal";
 import Sidebar from "./Sidebar";
 
 const Home = () => {
-  const [btnClick, setBtnClick] = useState(false);
-
-  const handleButtonClick = () => {};
   return (
     <section className="home-container">
       <FaBars className="fa-bars" />
       <Sidebar />
       {/*  <Modal />*/}
-      <button
-        className={`modal-btn ${btnClick ? "btn-click" : null}`}
-        onClick={handleButtonClick}
-      >
-        Show Modal
-      </button>
+      <button className={`modal-btn`}>Show Modal</button>
     </section>
   );
 };

2613466b7dc6ad113c56396af7add82733a3d87b
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-12-12 15:31:30 +0530

    SidebarModalContext created

--- src/context.js
+++ src/context.js
@@ -1,1 +1,22 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, createContext } from "react";
+
+/* --- 
+        This component has the gloabl data | our context.
+        We'll create our context using createContext()
+    ---*/
+
+export const SidebarModalContext = createContext();
+/* --- We have access to Provider now --- */
+
+export default function context({ children }) {
+  const [btnClick, setBtnClick] = useState(false);
+
+  const handleBtnClick = () => {
+    setBtnClick(true);
+  };
+  return (
+    <SidebarModalContext.Provider value={{ btnClick, handleBtnClick }}>
+      {children}
+    </SidebarModalContext.Provider>
+  );
+}
